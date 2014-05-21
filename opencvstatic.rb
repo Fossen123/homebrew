cad266af6d1224684f751268d982b926ec557ada
Andrea Tagliasacchi <andrea.tagliasacchi@gmail.com>
2014-05-21 17:45:19 +0200

    [opencv] corrected rule for OpenCV

--- opencvstatic.rb
+++ opencvstatic.rb
@@ -1,9 +1,9 @@
-require 'formula'
-class OpenCV_static < Formula
+require "formula"
+class Opencvstatic < Formula
   version "2.4.8"
   url 'https://github.com/drtaglia/homebrew/raw/master/opencv_static.tar.gz'
   homepage 'http://opencv.org/opencv-2-4-8.html'
-  sha1 'ad9ced9f278edfaca25d1b4ca679be70f73a48a3'
+  sha1 '4e95df02e3bd77cc65fbb75d58620548a529638a'
 
   def install
     lib.install Dir["lib/*"]
